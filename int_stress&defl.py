24c4db85a816333019fdb1b0492700b47cce27a4
Gavinsun <chuyansun@gmail.com>
2018-02-22 11:32:56 +0100

    function for calculating deflection

--- int_stress&defl.py
+++ int_stress&defl.py
@@ -6,8 +6,15 @@
 from math import *
 
 
-# TODO: blind built without specifications from other modules, change if inertia function is finalized
 # normal stress due to bending
 def norm_strs(M_z, I_z_z, y):
     sigma_x = M_z / I_z_z * y
     return sigma_x
+
+
+# deflection function
+# returns v prime prime
+def defl(M_x, I_z_z, E):
+    v = -1 * M_x / (E * I_z_z)
+    return v
+
